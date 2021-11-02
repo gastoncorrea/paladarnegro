1dba8b445c88e66fa957037fc788d63b81ba8176
Gastón <gastoncorrea90e@gmail.com>
2021-11-02 18:08:29 -0300

    Se pudo agregar boton hamburguesa al navBar

--- src/componentes/Navegacion.js
+++ src/componentes/Navegacion.js
@@ -4,16 +4,32 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 const Navegacion = () => {
   return (
     <Fragment>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="dark" variant="dark" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">Paladar Negro</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+      <Nav.Link href="#quienesSomos">Quienes somos</Nav.Link>
+              <Nav.Link href="#">Productos</Nav.Link>
+              <Nav.Link href="#">Contacto</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+      {/* <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Paladar Negro</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#quienesSomos">Quienes somos</Nav.Link>
-            <Nav.Link href="#features">Productos</Nav.Link>
-            <Nav.Link href="#pricing">Contacto</Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#quienesSomos">Quienes somos</Nav.Link>
+              <Nav.Link href="#features">Productos</Nav.Link>
+              <Nav.Link href="#pricing">Contacto</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
-      </Navbar>
+      </Navbar> */}
     </Fragment>
   );
 };
