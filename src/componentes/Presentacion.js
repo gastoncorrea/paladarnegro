c0c4aac8dfc969964e8e09233c64b5a450acd006
Gastón <gastoncorrea90e@gmail.com>
2021-11-02 22:33:16 -0300

    Se cargaron las 3 imagenes para el slider

--- src/componentes/Presentacion.js
+++ src/componentes/Presentacion.js
@@ -1,44 +1,39 @@
 import React, { Fragment } from "react";
-import {Carousel} from 'react-bootstrap';
+import { Carousel } from "react-bootstrap";
 const Presentacion = () => {
   return (
     <Fragment>
       <section class="text-center my-5">
-      <Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src={process.env.PUBLIC_URL+'tomahawk_y_tbone.png'}
-      alt="First slide"
-    />
-    <Carousel.Caption>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Second slide&bg=282c34"
-      alt="Second slide"
-    />
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={process.env.PUBLIC_URL + "tomahawk_y_tbone.png"}
+              alt="First slide"
+            />
+            <Carousel.Caption></Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={process.env.PUBLIC_URL + "entraña.png"}
+              alt="Second slide"
+            />
 
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Third slide&bg=20232a"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
+            <Carousel.Caption></Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={process.env.PUBLIC_URL + "osobuco.png"}
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>Probá lo Nuevo</h3>
+              <p>Disfruta de nuestro atrevimiento</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </section>
     </Fragment>
   );
