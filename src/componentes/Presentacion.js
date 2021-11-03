b440cb0608b041c053c0a27ca5c881c0acd0ed2e
Gastón <gastoncorrea90e@gmail.com>
2021-11-02 22:18:06 -0300

    Se agrego el 1er slider a la pagina principal

--- src/componentes/Presentacion.js
+++ src/componentes/Presentacion.js
@@ -8,12 +8,10 @@ const Presentacion = () => {
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="holder.js/800x400?text=First slide&bg=373940"
+      src={process.env.PUBLIC_URL+'tomahawk_y_tbone.png'}
       alt="First slide"
     />
     <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
