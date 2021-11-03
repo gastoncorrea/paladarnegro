6a69dc685c7cf5ccd499f59a6f756a440907e933
Gastón <gastoncorrea90e@gmail.com>
2021-11-02 23:28:11 -0300

    Se hizo sistema de grilla

--- src/componentes/Presentacion.js
+++ src/componentes/Presentacion.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
-import { Carousel } from "react-bootstrap";
+import { Carousel, Card, Button, Badge } from "react-bootstrap";
+
 const Presentacion = () => {
   return (
     <Fragment>
@@ -34,6 +35,68 @@ const Presentacion = () => {
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
+      </section>
+      <section className="my-5 row container mx-auto">
+        <div className="col-sm-12 col-md-3">
+          <Card>
+            <Card.Img
+              variant="top"
+              src={process.env.PUBLIC_URL + "colita_de_cuadril_canva.png"}
+            />
+            <Card.Body>
+              <Card.Title>Colita de Cuadril</Card.Title>
+              <Card.Text>
+                Delicioso corte, jugoso y Suave
+                <div>
+                  <Badge pill bg="warning" text="danger">
+                    <p>$1000</p>
+                  </Badge>
+                </div>
+              </Card.Text>
+              <Button variant="primary">Comprar</Button>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col-sm-12 col-md-3">
+          <Card>
+            <Card.Img
+              variant="top"
+              src={process.env.PUBLIC_URL + "colita_de_cuadril_canva.png"}
+            />
+            <Card.Body>
+              <Card.Title>Colita de Cuadril</Card.Title>
+              <Card.Text>Delicioso corte, jugoso y suave</Card.Text>
+              <Button variant="primary">Comprar</Button>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col-sm-12 col-md-3">
+          <Card>
+            <Card.Img
+              variant="top"
+              src={process.env.PUBLIC_URL + "colita_de_cuadril_canva.png"}
+            />
+            <Card.Body>
+              <Card.Title>Colita de Cuadril</Card.Title>
+              <Card.Text>Delicioso corte, jugoso y suave</Card.Text>
+              <Button variant="primary">Comprar</Button>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col-sm-12 col-md-3">
+          <Card>
+            <Card.Img
+              variant="top"
+              src={process.env.PUBLIC_URL + "colita_de_cuadril_canva.png"}
+              className="w-100"
+            />
+            <Card.Body>
+              <Card.Title>Colita de Cuadril</Card.Title>
+              <Card.Text>Delicioso corte, jugoso y suave</Card.Text>
+              <Button variant="primary">Comprar</Button>
+            </Card.Body>
+          </Card>
+        </div>
       </section>
     </Fragment>
   );
